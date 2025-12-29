09f51fc1d6b3abe6c7d61dae3ef30e31294521e0
Carsten Friedrich <carsten.friedrich@gmail.com>
2025-12-29 11:45:00 +1100

    Better metric logging and smaller training size

--- test_nn_q.py
+++ test_nn_q.py
@@ -7,36 +7,31 @@ from torch.utils.tensorboard import SummaryWriter
 from tic_tac_toe import RndMinMaxAgent
 from tic_tac_toe.Board import Board, GameResult, CROSS, NAUGHT, EMPTY
 from tic_tac_toe.RandomPlayer import RandomPlayer
+from tic_tac_toe.ReplayNNQPlayer import ReplayNNQPlayer
 from util import print_board, play_game, evaluate_batch
 # from tic_tac_toe.RandomPlayer import RandomPlayer
 from tic_tac_toe.MinMaxAgent import MinMaxAgent
 from tic_tac_toe.RndMinMaxAgent import RndMinMaxAgent
 # from tic_tac_toe.TabularQPlayer import TQPlayer
 from tic_tac_toe.SimpleNNQPlayer import NNQPlayer
+from tic_tac_toe.EGreedyNNQPlayer import EGreedyNNQPlayer
 import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # device = torch.device("cpu")
 
-
-# Define a descriptive name for the current experiment
-experiment_name = "NNQPlayer_vs_RndMinMaxAgent"
-# Generate a unique timestamp string
-timestamp = time.strftime("%Y%m%d-%H%M%S")
-# Combine them to create the unique log directory path
-log_dir_path = os.path.join("runs", f"{experiment_name}_{timestamp}")
-
-# Initialize the SummaryWriter with the specific path
-writer = SummaryWriter(log_dir_path)
-
 board = Board()
-nnplayer = NNQPlayer("QLearner1", device= device, writer=writer)
-nnplayer2 = NNQPlayer("QLearner2", device= device, writer=writer)
 
-deep_nnplayer = NNQPlayer("DeepQLearner1", device= device, writer=writer)
 
-rndplayer = RandomPlayer()
-mm_player = RndMinMaxAgent()
+nnegreedy_player = EGreedyNNQPlayer("EGreedyNNQPlayer", device= device)
+nnplayer = NNQPlayer("NNQPlayer", device= device)
+# nnplayer2 = NNQPlayer("NNQPlayer2", device= device, writer=writer)
+#
+
+rndplayer = RandomPlayer("RandomPlayer")
+mm_player = RndMinMaxAgent("RandomMinMaxPlayer")
+replayNNQPlayer = ReplayNNQPlayer("ReplayNNQPlayer", device= device)
+
 # tq_player = TQPlayer()
 
 p1_wins = []
@@ -45,39 +40,65 @@ draws = []
 game_number = []
 game_counter = 0
 
+games_per_training_batch = 40
+num_training_batches = 200
+num_training_eval_games = 50
+
 num_evaluation_batches = 2
 games_per_evaluation_batch = 100
-num_training_batches = 100
 
 # nnplayer rndplayer mm_player
-p1_t = nnplayer
-p2_t = mm_player
+p1 = nnplayer
+p2 = rndplayer
+
+# Define a descriptive name for the current experiment
+experiment_name = f"{p1.name}_vs_{p2.name}"
+# Generate a unique timestamp string
+timestamp = time.strftime("%Y%m%d-%H%M%S")
+# Combine them to create the unique log directory path
+log_dir_path = os.path.join("runs", f"{timestamp}_{experiment_name}")
+writer = SummaryWriter(log_dir_path)
+p1.writer = writer
+p2.writer = writer
+
+p1.log_graph()
+# Initialize the SummaryWriter with the specific path
 
 for i in range(num_training_batches):
-    p1win, p2win, draw = evaluate_batch(p1_t, p2_t, games_per_evaluation_batch, False
-                                        , writer=writer, epoch=i)
-    p1_t.log_weights()
-    p2_t.log_weights()
+    # Training phase
+    p1.training = True
+    p2.training = True
+    p1win, p2win, draw = evaluate_batch(p1, p2, games_per_training_batch, False, writer=writer, epoch=i)
+    p1_wins.append(p1win/games_per_training_batch)
+    p2_wins.append(p2win/games_per_training_batch)
+    draws.append(draw/games_per_training_batch)
+    p1.log_weights()
+    p2.log_weights()
 
-    p1_wins.append(p1win)
-    p2_wins.append(p2win)
-    draws.append(draw)
+    # Evaluation phase
+    # p1.training = False
+    # p2.training = False
+    # p1win, p2win, draw = evaluate_batch(p1, p2, num_training_eval_games, False, writer=writer, epoch=i)
+    # p1_wins.append(p1win/num_training_eval_games)
+    # p2_wins.append(p2win/num_training_eval_games)
+    # draws.append(draw/num_training_eval_games)
+
     game_counter = game_counter + 1
     game_number.append(game_counter)
 
 writer.close()
 
-nnplayer.training= False
-nnplayer2.training= False
+p1.training= False
+p2.training= False
 
-p1 = p1_t
-p2 = p2_t
+# p1 = nnegreedy_player
+# p2 = mm_player
 
 for i in range(num_evaluation_batches):
     p1win, p2win, draw = evaluate_batch(p1, p2, games_per_evaluation_batch, False)
-    p1_wins.append(p1win)
-    p2_wins.append(p2win)
-    draws.append(draw)
+    p1_wins.append(p1win/games_per_evaluation_batch)
+    p2_wins.append(p2win/games_per_evaluation_batch)
+    draws.append(draw/games_per_evaluation_batch)
     game_counter = game_counter + 1
     game_number.append(game_counter)
 
